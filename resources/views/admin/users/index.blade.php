66742d60cb80ea6aae097ff335229984490fe397
mladen <mladen.mandic1989@gmail.com>
2018-10-29 03:53:59 +0100

    users section working 100 percent - except security

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -8,6 +8,7 @@
     <thead>
       <tr>
         <th>ID</th>
+        <td>Photo</td> 
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -23,7 +24,9 @@
           
         <tr>
         <td>{{$user->id}}</td>
-        <td>{{$user->name}}</td>
+        
+        <td><img height=70 width=60 src="{{$user->photo ? $user->photo->file : 'http://placehold.it/400x400'}}"  alt=""></td>
+        <td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</a></td>
         <td>{{$user->email}}</td>
         <td>{{$user->role->name}}</td>
         <td>{{$user->is_active == 1 ? 'Active':'No Active'}}</td>
